2fc2ffa20b6d0da456e488e902a545c19c171ee9
David Molinero <dmolinero@ebay.com>
2016-12-28 22:23:27 +0000

    Update setup.sh

--- delivery-api/setup.sh
+++ delivery-api/setup.sh
@@ -2,3 +2,7 @@
 
 docker build . --tag kenfos-mysql
 docker run --name kenfos -p3306:3306 -d kenfos-mysql
+
+rm betamax.pem 2> /dev/null
+wget -q https://raw.githubusercontent.com/betamaxteam/betamax/master/betamax.pem
+keytool -importcert -keystore $JAVA_HOME/jre/lib/security/cacerts -file betamax.pem -alias betamax -storepass changeit -noprompt > /dev/null
