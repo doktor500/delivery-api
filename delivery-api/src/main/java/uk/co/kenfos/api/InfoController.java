2ceece0540639b1aeaaefa5d4fd77e1f5aa4c663
David Molinero <dmolinero@ebay.com>
2017-02-01 23:43:34 +0000

    Add healthcheck indicator

--- delivery-api/src/main/java/uk/co/kenfos/api/InfoController.java
+++ delivery-api/src/main/java/uk/co/kenfos/api/InfoController.java
@@ -1,10 +1,15 @@
 package uk.co.kenfos.api;
 
 import com.google.common.collect.ImmutableMap;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+
+import static java.lang.System.currentTimeMillis;
 
 @RestController
 public class InfoController {
@@ -12,5 +17,10 @@ public class InfoController {
     @RequestMapping("/")
     public Map info() {
         return ImmutableMap.of("info", "delivery-api");
+    }
+
+    @Bean
+    public HealthIndicator app() {
+        return () -> Health.status("OK").withDetail("timestamp", currentTimeMillis()).build();
     }
 }
